cef1387dfef02a787e1a7d77a2d83b387dc903a9
Seth Poulsen <poulsenseth@yahoo.com>
2021-12-07 15:43:11 -0600

    Prep and template code for implementing the next proof

--- theories/nums.v
+++ theories/nums.v
@@ -5,7 +5,7 @@ Require Import ZArith.
 
 Definition divide (a b : Z) : Prop := exists q : Z, b = (q * a)%Z.
 
-Notation "( x | y )" := (divide x y) (at level 0) : Z_scope.
+Notation "( x | y )" := (divide x y) (at level 0) : Z_scope .
 
 Local Open Scope Z_scope.
 
@@ -29,7 +29,6 @@ ring.
 PostExplain ring.
 Qed.
 
-
 Lemma divide_refl: forall a: Z, (a | a).
 Proof.
 intro x.
@@ -38,4 +37,51 @@ exists 1.
 ring.
 Qed.
 
+Lemma divide_mult_left : forall a b c : Z, (a | b) -> ((c * a)%Z | (c * b)%Z).
+Proof.
+intros. unfold divide.
+destruct H.
+exists x.
+rewrite H.
+ring.
+Qed.
 
+Lemma divide_mult_left_inst : forall a b c : Z, (a | b) -> ((c * a)%Z | (c * b)%Z).
+Proof.
+
+PreExplain intro a.
+intro a.
+PostExplain intro a.
+
+PreExplain intro b.
+intro b.
+PostExplain intro b.
+
+PreExplain intro c.
+intro c.
+PostExplain intro c.
+
+PreExplain unfold divide.
+unfold divide.
+PostExplain unfold divide.
+
+PreExplain intro H.
+intro H.
+PostExplain intro H.
+
+PreExplain destruct H.
+destruct H.
+PostExplain destruct H.
+
+PreExplain exists x.
+exists x.
+PostExplain exists x.
+
+PreExplain rewrite H.
+rewrite H.
+PostExplain rewrite H.
+
+PreExplain ring.
+ring.
+PostExplain ring.
+Qed.
